318efe743b854e5a5e20521ef903c356cb65eb9a
Feex <fortronin364@email.com>
2026-04-13 14:30:09 +0300

    1-11

--- solutions/10-spread-operator.js
+++ solutions/10-spread-operator.js
@@ -1,3 +1,18 @@
 // BEGIN
+const make = (name, massive = null) =>{
+    let result = {
+            name: name,
+            state: 'moderating',
+            createdAt: Date.now()
+        }
+    if (massive !== null){
+        let keys = Object.keys(massive);
+        for (let i = 0; i < keys.length; i++){
+            result[keys[i]] = massive[keys[i]]
+        }
+    }
+    return result;
+}
 
+export default make;
 // END
--- solutions/9-cloning.js
+++ solutions/9-cloning.js
@@ -1,3 +1,12 @@
 // BEGIN
+const cloneShallow = (data) => {
+    let result = {};
+    let keys = Object.keys(data);
+    for (let i = 0; i < keys.length; i++){
+        result[keys[i]] = data[keys[i]];
+    }
+    return result;
+}
 
+export default cloneShallow;
 // END
